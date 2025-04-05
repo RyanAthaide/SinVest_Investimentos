5ed9fd5f43e32e6756ab19bae7c79296f1c932db
RyanAthaide <athaideryan1@gmail.com>
2025-04-05 14:18:45 -0300

    alterando caminhos das imagens

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    safelist: [
+      "bg-[url('/assets/fundo-login.jpg')]"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
